6d493cf6cde4ab1174fe13a95196cf146502e07a
shand <shandrug.nikita@gmail.com>
2025-04-12 20:40:30 +0200

    Added metadata file

---
+++ metadata.py
@@ -0,0 +1,21 @@
+__title__ = "БYMEP"
+__description__ = """
+    Boomer is like your old car — easy to drive, and a joy every time.
+    It’s a universal SBOM (Software Bill of Materials) generator that lists all your project dependencies in a single, clean JSON. 
+    No need to specify the language — Boomer automatically detects it and uses the appropriate parser to gather the libraries.
+"""
+__version__ = "0.0.1"
+__license__ = "License: MIT"
+__url__ = "https://github.com/whitespots/boomer"
+__copyrights__ = "Copyright (c) 2025 Whitespots"
+__vendor__ = "whitespots/boomer"
+__bmw__ = """
+       _________________________   
+     /                          \     
+*%%_#____________________________#_%%*
+  /*\____________ () ____________/*\\
+ | / ]( )( )\[||||][||||]/( )( )[ \ |
+ |___________\__________/___________|
+ |___\___]__________________[___/___|
+ #########                  #########
+"""
